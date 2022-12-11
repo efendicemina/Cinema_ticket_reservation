cbf3661c3b5c18feaeaba820caaf24a42a5fdfbc
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-11 16:53:20 +0100

    Added MovieDao interface

---
+++ src/main/java/ba/unsa/etf/rpr/dao/MovieDao.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Movie;
+
+/**
+ * Dao interface for Flight domain bean
+ *
+ * @author Emina Efendic
+ */
+public interface MovieDao extends Dao<Movie> {
+}
