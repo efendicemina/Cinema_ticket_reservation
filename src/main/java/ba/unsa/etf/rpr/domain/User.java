7ae3d75f7f9ac05e7250719cb41487ba67bd99ac
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-11 16:42:30 +0100

    Added attributes, setters and getters class

--- src/main/java/ba/unsa/etf/rpr/domain/User.java
+++ src/main/java/ba/unsa/etf/rpr/domain/User.java
@@ -1,4 +1,67 @@
 package ba.unsa.etf.rpr.domain;
 
 public class User {
+    private int id;
+    private String name;
+    private String phone;
+    private String email;
+    private boolean admin;
+    private String username;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
