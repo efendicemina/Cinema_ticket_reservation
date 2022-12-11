9f338770f0016bf7080182a64aa9f6e33f1de745
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-11 16:52:12 +0100

    Added UserDao interface

---
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.User;
+
+/**
+ * Dao interface for User domain bean
+ *
+ * @author Emina Efendic
+ */
+public interface UserDao extends Dao<User> {
+}
