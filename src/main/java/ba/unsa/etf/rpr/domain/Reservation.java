b06ba8d4c01267979a8a853541268e3b177b2637
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-11 16:48:15 +0100

    Added attributes setters and getters

--- src/main/java/ba/unsa/etf/rpr/domain/Reservation.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Reservation.java
@@ -1,5 +1,49 @@
 package ba.unsa.etf.rpr.domain;
 
 public class Reservation {
+    private int id;
+    private User user;
+    private Movie movie;
+    private String sector;
+    private int tickets;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public int getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(int tickets) {
+        this.tickets = tickets;
+    }
 }
