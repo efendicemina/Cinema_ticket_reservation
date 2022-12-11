1f6c671874140127823ed59056aa8a1c279f5628
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-11 16:54:25 +0100

    Added ReservationDao interface

---
+++ src/main/java/ba/unsa/etf/rpr/dao/ReservationDao.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Reservation;
+
+/**
+ * Dao interface for Reservation domain bean
+ *
+ * @author Emina Efendic
+ */
+public interface ReservationDao extends Dao<Reservation> {
+}
