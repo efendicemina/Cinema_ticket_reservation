6e045f5a6e3800fd1cf56b7df0499898f346e5d1
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-11 16:46:48 +0100

    Added attributes setters and getters

--- src/main/java/ba/unsa/etf/rpr/domain/Movie.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Movie.java
@@ -1,4 +1,105 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.sql.Timestamp;
+
 public class Movie {
+    private int id;
+    private String name;
+    private String genre;
+    private Timestamp date_time;
+    private int duration;
+    private int r_price;
+    private int l_price;
+    private int p_price;
+    private int r_capacity;
+    private int l_capacity;
+    private int p_capacity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Timestamp getDate_time() {
+        return date_time;
+    }
+
+    public void setDate_time(Timestamp date_time) {
+        this.date_time = date_time;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getR_price() {
+        return r_price;
+    }
+
+    public void setR_price(int r_price) {
+        this.r_price = r_price;
+    }
+
+    public int getL_price() {
+        return l_price;
+    }
+
+    public void setL_price(int l_price) {
+        this.l_price = l_price;
+    }
+
+    public int getP_price() {
+        return p_price;
+    }
+
+    public void setP_price(int p_price) {
+        this.p_price = p_price;
+    }
+
+    public int getR_capacity() {
+        return r_capacity;
+    }
+
+    public void setR_capacity(int r_capacity) {
+        this.r_capacity = r_capacity;
+    }
+
+    public int getL_capacity() {
+        return l_capacity;
+    }
+
+    public void setL_capacity(int l_capacity) {
+        this.l_capacity = l_capacity;
+    }
+
+    public int getP_capacity() {
+        return p_capacity;
+    }
+
+    public void setP_capacity(int p_capacity) {
+        this.p_capacity = p_capacity;
+    }
 }
