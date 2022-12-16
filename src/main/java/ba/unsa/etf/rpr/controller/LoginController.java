0dcf58a8c44a75473f62461431f53ff23c796c58
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-16 18:19:14 +0100

    Added method for switching to register form

--- src/main/java/ba/unsa/etf/rpr/controller/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/LoginController.java
@@ -1,22 +1,23 @@
 package ba.unsa.etf.rpr.controller;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
 import javafx.stage.Stage;
-import java.io.File;
-import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ResourceBundle;
-import javafx.scene.image.Image;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import ba.unsa.etf.rpr.dao.UserDaoSQLImpl;
-public class LoginController implements Initializable {
+import javafx.stage.StageStyle;
+
+public class LoginController  {
     @FXML
     private Button cancelButton;
     @FXML
@@ -31,12 +32,15 @@ public class LoginController implements Initializable {
     private TextField usernameTextField;
     @FXML
     private TextField passwordField;
-    @Override
+    @FXML
+    private Hyperlink registerLink;
+  /*  @Override
     public void initialize(URL url, ResourceBundle resourceBundle){
         File file=new File("resources/images/movie.jpg");
         Image image=new Image(file.toURI().toString());
         image1.setImage(image);
     }
+      */
     public void CancelButtonOnAction(javafx.event.ActionEvent actionEvent) {
         Stage stage=(Stage) cancelButton.getScene().getWindow();
         stage.close();
@@ -48,24 +52,33 @@ public class LoginController implements Initializable {
         }
         else validateLogin();
     }
-    public void validateLogin(){
-        String insert = "SELECT count(1) from users where username='"+usernameTextField.getText()+"' AND password='"
-                +passwordField.getText()+"'";
-        try{
-            UserDaoSQLImpl u=new UserDaoSQLImpl();
+    public void validateLogin() {
+        String insert = "SELECT count(1) from users where username='" + usernameTextField.getText() + "' AND password='"
+                + passwordField.getText() + "'";
+        try {
+            UserDaoSQLImpl u = new UserDaoSQLImpl();
             PreparedStatement stmt = u.getConnection().prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
             ResultSet rs = stmt.executeQuery();
-            while (rs.next()){ // result set is iterator.
-                if(rs.getInt(1)==1){
+            while (rs.next()) { // result set is iterator.
+                if (rs.getInt(1) == 1) {
                     loginMessageLabel.setText("top");
-                }
-                else{
+                } else {
                     loginMessageLabel.setText("Invalid login.");
                 }
             }
             rs.close();
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+    public void registerLinkOnAction()throws Exception{
+        Stage registerStage=new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/register.fxml"));
+        registerStage.initStyle(StageStyle.UNDECORATED);
+        registerStage.setScene(new Scene(root, 520,593));
+        registerStage.setResizable(false);
+        registerStage.show();
+        Stage stage=(Stage) registerLink.getScene().getWindow();
+        stage.close();
     }
 }
