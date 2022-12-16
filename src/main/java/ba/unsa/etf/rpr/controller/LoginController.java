5403252f993cd808ba5693704bd5e1fdbfeee455
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-16 22:30:42 +0100

    Important changes in registerLinkOnAction

--- src/main/java/ba/unsa/etf/rpr/controller/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/LoginController.java
@@ -1,53 +1,41 @@
 package ba.unsa.etf.rpr.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Hyperlink;
 import javafx.stage.Stage;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Objects;
+
 import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import ba.unsa.etf.rpr.dao.UserDaoSQLImpl;
-import javafx.stage.StageStyle;
+
 
 public class LoginController  {
     @FXML
     private Button cancelButton;
     @FXML
-    private Button loginButton;
-    @FXML
     private Label loginMessageLabel;
-    @FXML
-    private ImageView image1;
-    @FXML
-    private ImageView image2;
+
     @FXML
     private TextField usernameTextField;
     @FXML
     private TextField passwordField;
-    @FXML
-    private Hyperlink registerLink;
-  /*  @Override
-    public void initialize(URL url, ResourceBundle resourceBundle){
-        File file=new File("resources/images/movie.jpg");
-        Image image=new Image(file.toURI().toString());
-        image1.setImage(image);
-    }
-      */
+
     public void CancelButtonOnAction(javafx.event.ActionEvent actionEvent) {
         Stage stage=(Stage) cancelButton.getScene().getWindow();
         stage.close();
     }
 
     public void loginButtonOnAction(javafx.event.ActionEvent actionEvent) {
-        if(usernameTextField.getText().isEmpty()!=false && passwordField.getText().isEmpty()!=false){
+        if(usernameTextField.getText().isEmpty() && passwordField.getText().isEmpty()){
             loginMessageLabel.setText("Invalid login.");
         }
         else validateLogin();
@@ -71,14 +59,11 @@ public class LoginController  {
             e.printStackTrace();
         }
     }
-    public void registerLinkOnAction()throws Exception{
-        Stage registerStage=new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/register.fxml"));
-        registerStage.initStyle(StageStyle.UNDECORATED);
-        registerStage.setScene(new Scene(root, 520,593));
-        registerStage.setResizable(false);
-        registerStage.show();
-        Stage stage=(Stage) registerLink.getScene().getWindow();
-        stage.close();
+    public void registerLinkOnAction(ActionEvent event)throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/register.fxml")));
+        Stage stage=(Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow();
+        Scene scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
