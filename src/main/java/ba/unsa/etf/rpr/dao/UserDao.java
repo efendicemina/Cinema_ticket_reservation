412e11e2f2983f61d1f2d780201c8ea04da5e0e4
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-20 21:49:38 +0100

    Refactoring of dao

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exception.MovieException;
 
 /**
  * Dao interface for User domain bean
@@ -8,4 +9,7 @@ import ba.unsa.etf.rpr.domain.User;
  * @author Emina Efendic
  */
 public interface UserDao extends Dao<User> {
+    public boolean checkUsernamePassword(String usernameTextField, String passwordField) throws MovieException;
+    public boolean findUsername(String usernameField) throws MovieException;
+
 }
