135d20dec7ee9eba81fe6025abf6302ae66d6bc6
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-20 21:27:24 +0100

    Added DaoFactory for refactoring of dao.

---
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -0,0 +1,29 @@
+package ba.unsa.etf.rpr.dao;
+
+/**
+ * Factory method for singleton implementation of DAOs.
+ *
+ * @author Emina Efendic
+ */
+public class DaoFactory {
+
+    private static final MovieDao movieDao = new MovieDaoSQLImpl();
+    private static final UserDao userDao = new UserDaoSQLImpl();
+    private static final ReservationDao reservationDao = new ReservationDaoSQLImpl();
+
+    private DaoFactory(){
+    }
+
+    public static MovieDao movieDao(){
+        return movieDao;
+    }
+
+    public static UserDao userDao(){
+        return userDao;
+    }
+
+    public static ReservationDao reservationDao(){
+        return reservationDao;
+    }
+
+}
