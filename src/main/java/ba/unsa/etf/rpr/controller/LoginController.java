dff5dd4f8b9e4d470b7e6266edd3d7af9e3ec70a
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-20 22:55:28 +0100

    Fixes in controller after dao refactoring

--- src/main/java/ba/unsa/etf/rpr/controller/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/LoginController.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr.controller;
 
+import ba.unsa.etf.rpr.exception.MovieException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -34,7 +35,7 @@ public class LoginController  {
         stage.close();
     }
 
-    public void loginButtonOnAction(javafx.event.ActionEvent actionEvent) {
+    public void loginButtonOnAction(javafx.event.ActionEvent actionEvent) throws MovieException {
         if(usernameTextField.getText().isEmpty() && passwordField.getText().isEmpty()){
             loginMessageLabel.setText("Invalid login.");
         }
