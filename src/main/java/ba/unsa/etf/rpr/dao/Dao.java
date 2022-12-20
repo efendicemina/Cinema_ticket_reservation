db790c17a11108cb3a965d24915cbaadd8d33073
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-20 21:17:34 +0100

    Refactoring of Dao

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.exception.MovieException;
+
 import java.util.List;
 
 /**
@@ -14,31 +16,31 @@ public interface Dao<T> {
      * @param id primary key of entity
      * @return Entity from database
      */
-    T getById(int id);
+    T getById(int id) throws MovieException;
 
     /**
      * Saves entity into database
      * @param item bean for saving to database
      * @return saved item with id field populated
      */
-    T add(T item);
+    T add(T item) throws MovieException;
 
     /**
      * Fully updates entity in database based on id (primary) match.
      * @param item - bean to be updated. id must be populated
      * @return updated version of bean
      */
-    T update(T item);
+    T update(T item) throws MovieException;
 
     /**
      * Hard delete of item from database with given id
      * @param id - primary key of entity
      */
-    void delete(int id);
+    void delete(int id) throws MovieException;
 
     /**
      * Lists all entities from database. WARNING: Very slow operation because it reads all records.
      * @return List of entities from database
      */
-    List<T> getAll();
+    List<T> getAll() throws MovieException;
 }
