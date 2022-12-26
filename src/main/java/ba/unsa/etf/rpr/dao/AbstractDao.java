2e83f6e038515bf43b2961ad75d6f221706359a0
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-26 12:49:52 +0100

    New refactor of Dao layer

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -2,13 +2,17 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Idable;
 import ba.unsa.etf.rpr.exception.MovieException;
+
 import java.sql.*;
 import java.util.*;
 
 /**
  * Abstract class that implements core DAO CRUD methods for every entity
+ *
+ * @author Dino Keco
  */
 public abstract class AbstractDao<T extends Idable> implements Dao<T>{
+
     private Connection connection;
     private String tableName;
 
@@ -22,9 +26,8 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T>{
             String password = p.getProperty("password");
             this.connection = DriverManager.getConnection(url, username, password);
         }catch (Exception e){
-            System.out.println("Connection to database not possible");
             e.printStackTrace();
-
+            System.exit(0);
         }
     }
 
@@ -32,43 +35,31 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T>{
         return this.connection;
     }
 
+    public void setConnection(Connection connection){
+        this.connection = connection;
+    }
+
+    /**
+     * Method for mapping ResultSet into Object
+     * @param rs - result set from database
+     * @return a Bean object for specific table
+     * @throws MovieException in case of error with db
+     */
     public abstract T row2object(ResultSet rs) throws MovieException;
 
+    /**
+     * Method for mapping Object into Map
+     * @param object - a bean object for specific table
+     * @return key, value sorted map of object
+     */
     public abstract Map<String, Object> object2row(T object);
 
     public T getById(int id) throws MovieException {
-        String query = "SELECT * FROM "+this.tableName+" WHERE id = ?";
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            stmt.setInt(1, id);
-            ResultSet rs = stmt.executeQuery();
-            if (rs.next()) { // result set is iterator.
-                T result = row2object(rs);
-                rs.close();
-                return result;
-            } else {
-                throw new MovieException("Object not found");
-            }
-        } catch (SQLException e) {
-            throw new MovieException(e.getMessage(), e);
-        }
+        return executeQueryUnique("SELECT * FROM "+this.tableName+" WHERE id = ?", new Object[]{id});
     }
 
     public List<T> getAll() throws MovieException {
-        String query = "SELECT * FROM "+ tableName;
-        List<T> results = new ArrayList<T>();
-        try{
-            PreparedStatement stmt = getConnection().prepareStatement(query);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()){ // result set is iterator.
-                T object = row2object(rs);
-                results.add(object);
-            }
-            rs.close();
-            return results;
-        }catch (SQLException e){
-            throw new MovieException(e.getMessage(), e);
-        }
+        return executeQuery("SELECT * FROM "+ tableName, null);
     }
 
     public void delete(int id) throws MovieException {
@@ -139,6 +130,48 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T>{
     }
 
     /**
+     * Utility method for executing any kind of query
+     * @param query - SQL query
+     * @param params - params for query
+     * @return List of objects from database
+     * @throws MovieException in case of error with db
+     */
+    public List<T> executeQuery(String query, Object[] params) throws MovieException{
+        try {
+            PreparedStatement stmt = getConnection().prepareStatement(query);
+            if (params != null){
+                for(int i = 1; i <= params.length; i++){
+                    stmt.setObject(i, params[i-1]);
+                }
+            }
+            ResultSet rs = stmt.executeQuery();
+            ArrayList<T> resultList = new ArrayList<>();
+            while (rs.next()) {
+                resultList.add(row2object(rs));
+            }
+            return resultList;
+        } catch (SQLException e) {
+            throw new MovieException(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Utility for query execution that always return single record
+     * @param query - query that returns single record
+     * @param params - list of params for sql query
+     * @return Object
+     * @throws MovieException in case when object is not found
+     */
+    public T executeQueryUnique(String query, Object[] params) throws MovieException{
+        List<T> result = executeQuery(query, params);
+        if (result != null && result.size() == 1){
+            return result.get(0);
+        }else{
+            throw new MovieException("Object not found");
+        }
+    }
+
+    /**
      * Accepts KV storage of column names and return CSV of columns and question marks for insert statement
      * Example: (id, name, date) ?,?,?
      */
@@ -157,13 +190,13 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T>{
                 questions.append(",");
             }
         }
-        return new AbstractMap.SimpleEntry<String,String>(columns.toString(), questions.toString());
+        return new AbstractMap.SimpleEntry<>(columns.toString(), questions.toString());
     }
 
     /**
      * Prepare columns for update statement id=?, name=?, ...
-     * @param row
-     * @return
+     * @param row - row to be converted intro string
+     * @return String for update statement
      */
     private String prepareUpdateParts(Map<String, Object> row){
         StringBuilder columns = new StringBuilder();
