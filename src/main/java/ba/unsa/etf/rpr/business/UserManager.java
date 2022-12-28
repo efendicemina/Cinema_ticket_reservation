868311d7da0ea735d930b1c3795f161c7e4791df
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-28 21:06:56 +0100

    Business logic implementation

--- src/main/java/ba/unsa/etf/rpr/business/UserManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/UserManager.java
@@ -1,4 +1,30 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Reservation;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exception.MovieException;
+
+import java.util.List;
+
 public class UserManager {
+    public void delete(int categoryId) throws MovieException {
+        try{
+            DaoFactory.userDao().delete(categoryId);
+        }catch (MovieException e){
+            if (e.getMessage().contains("FOREIGN KEY")){
+                throw new MovieException("NO");
+            }
+            throw e;
+        }
+
+    }
+
+    public User update(User cat) throws MovieException{
+        return DaoFactory.userDao().update(cat);
+    }
+
+    public List<User> getAll() throws MovieException{
+        return DaoFactory.userDao().getAll();
+    }
 }
