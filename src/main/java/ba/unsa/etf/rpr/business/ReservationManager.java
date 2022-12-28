75a11e0f4a2d8bb51a2fac7994a49ec00fe73392
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-28 21:07:23 +0100

    Business logic implementation

--- src/main/java/ba/unsa/etf/rpr/business/ReservationManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ReservationManager.java
@@ -1,4 +1,32 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Movie;
+import ba.unsa.etf.rpr.domain.Reservation;
+import ba.unsa.etf.rpr.exception.MovieException;
+
+import java.util.List;
+
 public class ReservationManager {
+
+
+    public void delete(int categoryId) throws MovieException {
+        try{
+            DaoFactory.reservationDao().delete(categoryId);
+        }catch (MovieException e){
+            if (e.getMessage().contains("FOREIGN KEY")){
+                throw new MovieException("NO");
+            }
+            throw e;
+        }
+
+    }
+
+    public Reservation update(Reservation cat) throws MovieException{
+        return DaoFactory.reservationDao().update(cat);
+    }
+
+    public List<Reservation> getAll() throws MovieException{
+        return DaoFactory.reservationDao().getAll();
+    }
 }
