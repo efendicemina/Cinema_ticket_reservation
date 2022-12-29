31c176297df2ca90d5bea6456ef0d6730c00c378
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-29 21:40:44 +0100

    Singleton pattern fix

--- src/main/java/ba/unsa/etf/rpr/dao/MovieDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/MovieDaoSQLImpl.java
@@ -16,7 +16,7 @@ public class MovieDaoSQLImpl extends AbstractDao<Movie> implements MovieDao{
     public Movie row2object(ResultSet rs) throws MovieException {
         try {
             Movie movie = new Movie();
-            movie.setId(rs.getInt("movie_id"));
+            movie.setId(rs.getInt("id"));
             movie.setName(rs.getString("name"));
             movie.setGenre(rs.getString("genre"));
             movie.setDate_time(rs.getTimestamp("date_time").toLocalDateTime());
@@ -28,17 +28,15 @@ public class MovieDaoSQLImpl extends AbstractDao<Movie> implements MovieDao{
     }
 
     @Override
-    public Map<String, Object> object2row(Movie object) throws MovieException {
-        try {
+    public Map<String, Object> object2row(Movie object) {
+
             Map<String, Object> item = new TreeMap<>();
-            item.put("movie_id", object.getId());
+            item.put("id", object.getId());
             item.put("name", object.getName());
             item.put("genre", object.getGenre());
             item.put("date_time", object.getDate_time());
             item.put("duration", object.getDuration());
             return item;
-        } catch (Exception e) {
-            throw new MovieException(e.getMessage(), e);
-        }
+
     }
 }
