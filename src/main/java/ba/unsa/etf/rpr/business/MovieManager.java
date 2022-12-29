b62d1b28933b18ce8becb7be474b587094319d82
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-29 21:41:34 +0100

    Adding validations

--- src/main/java/ba/unsa/etf/rpr/business/MovieManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/MovieManager.java
@@ -4,6 +4,8 @@ import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Movie;
 import ba.unsa.etf.rpr.exception.MovieException;
 import ba.unsa.etf.rpr.exception.MovieException;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
 
 import java.util.List;
 
@@ -11,12 +13,12 @@ import java.util.List;
 public class MovieManager {
 
 
-    public void delete(int categoryId) throws MovieException{
+    public void delete(int movieId) throws MovieException{
         try{
-            DaoFactory.movieDao().delete(categoryId);
+            DaoFactory.movieDao().delete(movieId);
         }catch (MovieException e){
             if (e.getMessage().contains("FOREIGN KEY")){
-                throw new MovieException("NO");
+                throw new MovieException("Cannot delete this movie since tickets have already been reserved.");
             }
             throw e;
         }
@@ -30,11 +32,20 @@ public class MovieManager {
     public List<Movie> getAll() throws MovieException{
         return DaoFactory.movieDao().getAll();
     }
-    public void validateFields(String name, String genre, String datetime, String duration) throws MovieException {
-        if(name.isEmpty() || genre.isEmpty() || datetime.isEmpty() || duration.isEmpty())
-            throw  new MovieException("To successfully add a movie all fields must be filled in!");
+    public void validateAddFields(String name, String genre, String datetime, Integer duration,
+                                  Integer min, Integer hour) throws MovieException {
+        if(name.isEmpty() || genre.isEmpty() || datetime.isEmpty() || duration==null
+        || min==null || hour==null)
+            throw  new MovieException("To successfully perform an action, all fields must be filled in!");
+    }
+     public void validateDeleteFields(Integer id) throws MovieException {
+        if(id==null)
+            throw  new MovieException("To successfully perform an action, id field must be filled in!");
     }
     public Movie add(Movie m) throws MovieException{
         return DaoFactory.movieDao().add(m);
+    }
+    public  Movie getById(int id) throws MovieException{
+        return DaoFactory.movieDao().getById(id);
     }
 }
