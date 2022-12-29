fe34993ab30df9d78fd72866101a3ee7e60be788
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-29 21:42:25 +0100

    Small changes

--- src/main/java/ba/unsa/etf/rpr/controller/AddMovieController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/AddMovieController.java
@@ -1,19 +1,13 @@
 package ba.unsa.etf.rpr.controller;
 
 import ba.unsa.etf.rpr.business.MovieManager;
-import ba.unsa.etf.rpr.dao.MovieDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Movie;
 import ba.unsa.etf.rpr.exception.MovieException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-
-import java.lang.Object.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import ba.unsa.etf.rpr.controller.AdminController;
 import javafx.scene.layout.Pane;
 
 
@@ -45,19 +39,10 @@ public class AddMovieController {
         minBox.getItems().addAll(min);
         durationBox.getItems().addAll(duration);
     }
-    private void showAlertWithoutHeaderText() {
-        Alert alert = new Alert(Alert.AlertType.WARNING);
-        alert.setTitle("Warning alert");
-
-        // Header Text: null
-        alert.setHeaderText(null);
-        alert.setContentText("In order to add a movie, all fields must be filled in");
-
-        alert.showAndWait();
-    }
     public void okButtonOnAction(ActionEvent actionEvent) throws MovieException {
         try {
-            movieManager.validateFields(nameField.getText(), genreField.getText(), dateField.toString(), durationBox.toString());
+            movieManager.validateAddFields(nameField.getText(), genreField.getText(), dateField.toString(), durationBox.getValue(),
+                    minBox.getValue(), hourBox.getValue());
             AdminController admin = new AdminController();
             LocalDate localDate = dateField.getValue();
             LocalDateTime localDateTime = localDate.atTime(hourBox.getValue(), minBox.getValue());
