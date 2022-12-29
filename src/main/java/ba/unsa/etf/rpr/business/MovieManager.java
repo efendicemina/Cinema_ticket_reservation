0a5d09a9195015ae7d74b42a2a33d402bbefa780
Emina Efendic <eefendic1@etf.unsa.ba>
2022-12-29 15:11:28 +0100

    Adding validation

--- src/main/java/ba/unsa/etf/rpr/business/MovieManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/MovieManager.java
@@ -30,5 +30,11 @@ public class MovieManager {
     public List<Movie> getAll() throws MovieException{
         return DaoFactory.movieDao().getAll();
     }
-
+    public void validateFields(String name, String genre, String datetime, String duration) throws MovieException {
+        if(name.isEmpty() || genre.isEmpty() || datetime.isEmpty() || duration.isEmpty())
+            throw  new MovieException("To successfully add a movie all fields must be filled in!");
+    }
+    public Movie add(Movie m) throws MovieException{
+        return DaoFactory.movieDao().add(m);
+    }
 }
