dc7f6e14fae2e98475c057047e97316a5f3af681
Emina Efendic <eefendic1@etf.unsa.ba>
2023-01-01 20:47:20 +0100

    Added ButtonCellFactory for booking

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/components/ButtonCellFactory.java
@@ -0,0 +1,30 @@
+package ba.unsa.etf.rpr.controllers.components;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.util.Callback;
+
+/**
+ * Button cell factory for creation of buttons for each cell in the table
+ * @param <T>
+ */
+public class ButtonCellFactory<T> implements Callback<TableColumn<T, T>, TableCell<T, T>> {
+
+    private final EventHandler<ActionEvent> buttonOne;
+
+
+    /**
+     *
+     * @param buttonOne - event handler button (BOOK)
+     */
+    public ButtonCellFactory(EventHandler<ActionEvent> buttonOne){
+        this.buttonOne = buttonOne;
+    }
+
+    @Override
+    public TableCell<T, T> call(TableColumn<T, T> quoteObjectTableColumn) {
+        return new ButtonTableCell<>(buttonOne);
+    }
+}
