f3e97aadbf15164e021eb9645c27838c386547a9
Emina Efendic <eefendic1@etf.unsa.ba>
2023-01-05 16:30:29 +0100

    Refactoring due to business logic

--- src/main/java/ba/unsa/etf/rpr/business/ReservationManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ReservationManager.java
@@ -5,6 +5,7 @@ import ba.unsa.etf.rpr.domain.Movie;
 import ba.unsa.etf.rpr.domain.Reservation;
 import ba.unsa.etf.rpr.exception.MovieException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReservationManager {
@@ -28,5 +29,21 @@ public class ReservationManager {
 
     public List<Reservation> getAll() throws MovieException{
         return DaoFactory.reservationDao().getAll();
+    }
+    public List<String> getBookedSeats(Integer id) throws MovieException {
+        List<String> booked=new ArrayList<>();
+        List<Reservation> all=getAll();
+        for(int i=0; i< all.size();i++) {
+            if(all.get(i).getId()==id)
+            booked.add(all.get(i).getSector());
+        }
+        return booked;
+    }
+    public Reservation add(Reservation r) throws MovieException{
+        return DaoFactory.reservationDao().add(r);
+    }
+    public void validateSeatField(String seat) throws MovieException {
+        if(seat==null)
+            throw  new MovieException("To successfully book, seat field must be filled in!");
     }
 }
