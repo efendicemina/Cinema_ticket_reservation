8e2ed6c277f05542c009c8417e8d9c370872b1c5
Emina Efendic <eefendic1@etf.unsa.ba>
2023-01-06 17:48:04 +0100

    Refactoring of dao

--- src/main/java/ba/unsa/etf/rpr/dao/MovieDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/MovieDaoSQLImpl.java
@@ -4,16 +4,40 @@ import ba.unsa.etf.rpr.domain.Movie;
 import ba.unsa.etf.rpr.exception.MovieException;
 
 import java.sql.ResultSet;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class MovieDaoSQLImpl extends AbstractDao<Movie> implements MovieDao{
-    public MovieDaoSQLImpl(){
-      super("movies");
+/**
+ * MySQL Implementation of DAO
+ * @author Emina Efendic
+ */
+public class MovieDaoSQLImpl extends AbstractDao<Movie> implements MovieDao {
+
+    private static MovieDaoSQLImpl instance = null;
+    private MovieDaoSQLImpl() {
+        super("movies");
+    }
+
+    /**
+     * @author Emina Efendic
+     * @return QuoteDaoSQLImpl
+     * We don't need more than one object for CRUD operations on table 'quotes' so we will use Singleton
+     * This method will call private constructor in instance==null and then return that instance
+     */
+    public static MovieDaoSQLImpl getInstance(){
+        if(instance==null)
+            instance = new MovieDaoSQLImpl();
+        return instance;
+    }
+
+    public static void removeInstance(){
+        if(instance!=null)
+            instance=null;
     }
 
     @Override
-    public Movie row2object(ResultSet rs) throws MovieException {
+    public Movie row2object(ResultSet rs) throws MovieException{
         try {
             Movie movie = new Movie();
             movie.setId(rs.getInt("id"));
@@ -27,16 +51,18 @@ public class MovieDaoSQLImpl extends AbstractDao<Movie> implements MovieDao{
         }
     }
 
+    /**
+     * @param object - object to be mapped
+     * @return map representation of object
+     */
     @Override
     public Map<String, Object> object2row(Movie object) {
-
-            Map<String, Object> item = new TreeMap<>();
-            item.put("id", object.getId());
-            item.put("name", object.getName());
-            item.put("genre", object.getGenre());
-            item.put("date_time", object.getDate_time());
-            item.put("duration", object.getDuration());
-            return item;
-
+        Map<String, Object> item = new TreeMap<>();
+        item.put("id", object.getId());
+        item.put("name", object.getName());
+        item.put("genre", object.getGenre());
+        item.put("date_time", object.getDate_time());
+        item.put("duration", object.getDuration());
+        return item;
     }
 }
