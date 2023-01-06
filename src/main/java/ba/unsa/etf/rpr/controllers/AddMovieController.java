a73e74e64e129a7a52fb716d7f1c0faa55085a02
Emina Efendic <eefendic1@etf.unsa.ba>
2023-01-06 17:48:51 +0100

    Adding dialog window

--- src/main/java/ba/unsa/etf/rpr/controllers/AddMovieController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AddMovieController.java
@@ -5,10 +5,21 @@ import ba.unsa.etf.rpr.domain.Movie;
 import ba.unsa.etf.rpr.exception.MovieException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Objects;
+
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 
 public class AddMovieController {
@@ -27,18 +38,20 @@ public class AddMovieController {
     DatePicker dateField;
     @FXML
     Label addMovieMessage;
-    MovieManager movieManager=new MovieManager();
+    MovieManager movieManager = new MovieManager();
     @FXML
     public Pane aouMoviePane;
     private Integer[] hour = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
     private Integer[] min = {00, 10, 20, 30, 40, 50};
     private Integer[] duration = {60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220};
+
     @FXML
-    public void initialize(){
+    public void initialize() {
         hourBox.getItems().addAll(hour);
         minBox.getItems().addAll(min);
         durationBox.getItems().addAll(duration);
     }
+
     public void okButtonOnAction(ActionEvent actionEvent) throws MovieException {
         try {
             movieManager.validateAddFields(nameField.getText(), genreField.getText(), dateField.toString(), durationBox.getValue(),
@@ -48,9 +61,22 @@ public class AddMovieController {
             LocalDateTime localDateTime = localDate.atTime(hourBox.getValue(), minBox.getValue());
             Movie movie = admin.createMovie(nameField.getText(), genreField.getText(), localDateTime, durationBox.getValue());
             movieManager.add(movie);
-            addMovieMessage.setText("Movie successfully added.");
-      }catch (Exception e){
-        new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
-     }
+            openDialog("Information", "/fxml/information.fxml");
+            Stage stage=(Stage) hourBox.getScene().getWindow();
+            stage.close();
+
+        } catch (Exception e) {
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
+    }
+
+    private void openDialog(String title,String file ) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(file));
+        Stage stage = new Stage();
+        stage.setScene(new Scene((Parent) loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setTitle(title);
+        stage.initStyle(StageStyle.UTILITY);
+        stage.show();
+
     }
 }
