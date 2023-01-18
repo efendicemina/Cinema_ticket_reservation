22132a0c72b87c486f6c5b9a133cd928afa1fccd
Emina Efendic <eefendic1@etf.unsa.ba>
2023-01-18 21:47:01 +0100

    Adding javadoc

--- src/main/java/ba/unsa/etf/rpr/exception/MovieException.java
+++ src/main/java/ba/unsa/etf/rpr/exception/MovieException.java
@@ -1,11 +1,15 @@
 package ba.unsa.etf.rpr.exception;
 
+/**
+ * MovieException is a custom exception class for handling errors related to movie operations.
+ * It extends the base Exception class and provides two constructors for creating instances of the exception.
+ * @author Emina Efendic
+ */
 public class MovieException extends Exception{
 
     public MovieException(String msg, Exception reason){
         super(msg, reason);
     }
-
     public MovieException(String msg){
         super(msg);
     }
