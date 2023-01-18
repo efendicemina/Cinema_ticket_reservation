1248e34d3cfe5d65c758af9eafcbe7ceed83883b
Emina Efendic <eefendic1@etf.unsa.ba>
2023-01-18 21:49:32 +0100

    Adding javadoc

--- src/main/java/ba/unsa/etf/rpr/controllers/AddMovieController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AddMovieController.java
@@ -2,26 +2,26 @@ package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.business.MovieManager;
 import ba.unsa.etf.rpr.domain.Movie;
-import ba.unsa.etf.rpr.exception.MovieException;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Objects;
-
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
-
+/**
+ *
+ AddMovieController class is responsible for adding new movies to the system.
+ It provides functionality for creating new movie object by providing its name, genre, date and time of showing, and duration of the movie.
+ It also performs validation of user input and displays error messages in case of invalid input.
+ @author Emina Efendic
+ */
 public class AddMovieController {
 
     @FXML
@@ -39,20 +39,25 @@ public class AddMovieController {
     @FXML
     Label addMovieMessage;
     MovieManager movieManager = new MovieManager();
-    @FXML
-    public Pane aouMoviePane;
-    private Integer[] hour = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
-    private Integer[] min = {00, 10, 20, 30, 40, 50};
-    private Integer[] duration = {60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220};
+    private final Integer[] hour = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
+    private final Integer[] min = {0, 10, 20, 30, 40, 50};
+    private final Integer[] duration = {60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220};
 
+    /**
+     * Initializes the hourBox, minBox, and durationBox with predefined values for hours, minutes, and duration of the movie.
+     */
     @FXML
     public void initialize() {
         hourBox.getItems().addAll(hour);
         minBox.getItems().addAll(min);
         durationBox.getItems().addAll(duration);
     }
-
-    public void okButtonOnAction(ActionEvent actionEvent) throws MovieException {
+    /**
+     * Handles the 'OK' button click event, creates a new movie object using user input, adds it to the system and displays a confirmation dialog.
+     * In case of invalid input, it displays an error message.
+     *
+     */
+    public void okButtonOnAction() {
         try {
             movieManager.validateAddFields(nameField.getText(), genreField.getText(), dateField.toString(), durationBox.getValue(),
                     minBox.getValue(), hourBox.getValue());
@@ -61,7 +66,7 @@ public class AddMovieController {
             LocalDateTime localDateTime = localDate.atTime(hourBox.getValue(), minBox.getValue());
             Movie movie = admin.createMovie(nameField.getText(), genreField.getText(), localDateTime, durationBox.getValue());
             movieManager.add(movie);
-            openDialog("Information", "/fxml/information.fxml");
+            openDialog();
             Stage stage=(Stage) hourBox.getScene().getWindow();
             stage.close();
 
@@ -69,12 +74,14 @@ public class AddMovieController {
             new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
         }
     }
-
-    private void openDialog(String title,String file ) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource(file));
+    /**
+     * Opens a dialog window with the provided title and FXML file path
+     */
+    private void openDialog() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/information.fxml"));
         Stage stage = new Stage();
-        stage.setScene(new Scene((Parent) loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        stage.setTitle(title);
+        stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setTitle("Information");
         stage.initStyle(StageStyle.UTILITY);
         stage.show();
 
